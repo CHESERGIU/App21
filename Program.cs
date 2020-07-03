9b67dfa6c260cca512d2effd03fa80339e7ca40f
Che Sergiu <37384588+CHESERGIU@users.noreply.github.com>
2020-07-03 10:53:30 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace App21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] city = new string[] { "Paris", "Londra", "Madrid", "Berlin", "Roma" };
+            string inputData = Console.ReadLine();
+
+            switch (inputData)
+            {
+                case "Paris":
+                    Console.WriteLine("Franța");
+                    break;
+                case "Londra":
+                    Console.WriteLine("Marea Britanie");
+                    break;
+                case "Madrid":
+                    Console.WriteLine("Spania");
+                    break;
+                case "Berlin":
+                    Console.WriteLine("Germania");
+                    break;
+                case "Roma":
+                    Console.WriteLine("Italia");
+                    break;
+                default:
+                    Console.WriteLine("oras invalid");
+                    break;
+            }
+        }
+    }
+}
